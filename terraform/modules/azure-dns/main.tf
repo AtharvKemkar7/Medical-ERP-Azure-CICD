71ef684d187053b118d19e9a3724efab66566a46
Atharv Kemkar <atharvkemkar7@gmail.com>
2026-09-16 23:06:32 +0530

    fixed

--- terraform/modules/azure-dns/main.tf
+++ terraform/modules/azure-dns/main.tf
@@ -10,43 +10,43 @@ resource "azurerm_dns_zone" "main" {
 }
 
 resource "azurerm_dns_a_record" "root" {
+  count = var.ingress_ip_address != "" ? 1 : 0
+
   name                = "@"
   zone_name           = azurerm_dns_zone.main.name
   resource_group_name = var.resource_group_name
   ttl                 = 300
   records             = [var.ingress_ip_address]
-
-  count = var.ingress_ip_address != "" ? 1 : 0
 }
 
 resource "azurerm_dns_cname_record" "frontend" {
+  count = var.frontend_endpoint != "" ? 1 : 0
+
   name                = "www"
   zone_name           = azurerm_dns_zone.main.name
   resource_group_name = var.resource_group_name
   ttl                 = 300
-  record              = trimsuffix(
+
+  record = trimsuffix(
     replace(var.frontend_endpoint, "https://", ""),
     "/"
   )
-
-  count = var.frontend_endpoint != "" ? 1 : 0
 }
 
 resource "azurerm_dns_cname_record" "additional" {
-  for_each = {
+  for_each = var.frontend_endpoint != "" ? {
     for domain in var.domain_names :
     domain => domain
     if domain != var.domain_name
-  }
+  } : {}
 
   name                = trimsuffix(each.value, ".")
   zone_name           = azurerm_dns_zone.main.name
   resource_group_name = var.resource_group_name
   ttl                 = 300
-  record              = trimsuffix(
+
+  record = trimsuffix(
     replace(var.frontend_endpoint, "https://", ""),
     "/"
   )
-
-  count = var.frontend_endpoint != "" ? 1 : 0
 }
